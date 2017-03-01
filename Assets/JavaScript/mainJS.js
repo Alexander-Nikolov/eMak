dbeff276903d8e62e349867e66f1c85f7f30a67e
Alexander Nikolov <al.a.nikolov@abv.bg>
2017-03-01 14:10:02 +0200

    Did some finishing touches on the header

--- Assets/JavaScript/mainJS.js
+++ Assets/JavaScript/mainJS.js
@@ -1,17 +1,21 @@
 var navigation = document.getElementById('navigation');
 var fixedNav = document.getElementById('fixedNav');
 window.addEventListener('scroll', function(e) {
-    console.log(wrapper.scrollTop);
-    if (window.scrollY >= 200) {
+    if (window.scrollY >= 200 && window.innerWidth >= '1170') {
         navigation.style.position = 'fixed';
         navigation.style.top = '0px';
         navigation.style.width = '1170px';
-        fixedNav.style.display = 'block';
+        navigation.style.zIndex = '2';
+        navigation.style.top = '5px';
+        fixedNav.style.opacity = '0.9';
+        fixedNav.style.zIndex = '2';
     }
 
     if (window.scrollY < 200) {
         navigation.style.position = 'static';
         navigation.style.width = '100%';
-        fixedNav.style.display = 'none';
+        navigation.style.zIndex = '1';
+        fixedNav.style.opacity = '0';
+        fixedNav.style.zIndex = '-1';
     }
 })
