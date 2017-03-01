bc363ef14fd290450e655409a1ed0bd638f58e84
Alexander Nikolov <al.a.nikolov@abv.bg>
2017-03-01 16:43:27 +0200

    Small changes on the main section. Added back to home button

--- Assets/JavaScript/mainJS.js
+++ Assets/JavaScript/mainJS.js
@@ -1,6 +1,7 @@
 var navigation = document.getElementById('navigation');
 var fixedNav = document.getElementById('fixedNav');
-window.addEventListener('scroll', function(e) {
+var toHome = document.getElementById('toHome');
+window.addEventListener('scroll', function (e) {
     if (window.scrollY >= 200 && window.innerWidth >= '1170') {
         navigation.style.position = 'fixed';
         navigation.style.top = '0px';
@@ -9,13 +10,17 @@ window.addEventListener('scroll', function(e) {
         navigation.style.top = '5px';
         fixedNav.style.opacity = '0.9';
         fixedNav.style.zIndex = '2';
-    }
-
-    if (window.scrollY < 200) {
+    } else {
         navigation.style.position = 'static';
         navigation.style.width = '100%';
         navigation.style.zIndex = '1';
         fixedNav.style.opacity = '0';
         fixedNav.style.zIndex = '-1';
+    }
+
+    if (window.scrollY >= 250) {
+        toHome.style.bottom = '10px';
+    } else {
+        toHome.style.bottom = '-40px';
     }
 })
