ad2a9fe3d5f0de4fa4d2f30781fe67f5abcbf472
Alexander Nikolov <al.a.nikolov@abv.bg>
2017-03-02 22:34:43 +0200

    JavaScript code moved to the jQuery file and upgraded to jQuery

--- Assets/JavaScript/jquery.js
+++ Assets/JavaScript/jquery.js
@@ -1,5 +1,40 @@
+/*************************
+Scroll
+*************************/
+
 $("#toHome").click(function () {
     $('html, body').animate({
-        scrollTop:  $("header").offset().top - 20
+        scrollTop: $("header").offset().top - 20
     }, 1000);
 });
+
+
+
+/*************************
+Navigation hide + to home button
+*************************/
+$("#fixedNav").hide();
+var navShow = false;
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 230 && !navShow) {
+        navShow = true;
+        $("#fixedNav").fadeTo(500, 0.9);
+        $("#navigation").hide().fadeIn(1000).css({
+            position: 'fixed'
+
+        });
+        $('#toHome').stop().animate({
+            bottom: 10
+        }, 50)
+    } else if ($(window).scrollTop() < 230) {
+        navShow = false;
+        $("#fixedNav").hide();
+        $("#navigation").css({
+            'position': 'static'
+        })
+        $('#toHome').stop().animate({
+            bottom: -40
+        }, 50)
+    }
+
+})
