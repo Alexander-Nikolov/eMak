4d5493f7385224fcc5f77442078fa6638f3400a5
Alexander Nikolov <al.a.nikolov@abv.bg>
2017-03-03 18:50:22 +0200

    Side navigation is now fixed upon scroll

--- Assets/JavaScript/jquery.js
+++ Assets/JavaScript/jquery.js
@@ -18,6 +18,9 @@ var navShow = false;
 $(window).scroll(function () {
     if ($(window).scrollTop() > 230 && !navShow && window.innerWidth >= '1170') {
         navShow = true;
+        $("body").css({
+            "margin-top": "75px"
+        })
         $("#fixedNav").fadeTo(500, 0.9);
         $("#navigation").hide().fadeIn(1000).css({
             position: 'fixed'
@@ -28,13 +31,27 @@ $(window).scroll(function () {
         }, 50)
     } else if ($(window).scrollTop() < 230) {
         navShow = false;
+        $("body").css({
+            "margin-top": "20px"
+        })
         $("#fixedNav").hide();
         $("#navigation").css({
-            'position': 'static'
+            position: 'static'
         })
         $('#toHome').stop().animate({
             bottom: -40
         }, 50)
+    }
+
+    if ($(window).scrollTop() > 550 && window.innerWidth >= '1170') {
+        $("#sideMenu").css({
+            position: 'fixed',
+            top: '100px'
+        })
+    } else {
+        $("#sideMenu").css({
+            position: 'static'
+        })
     }
 
 })
