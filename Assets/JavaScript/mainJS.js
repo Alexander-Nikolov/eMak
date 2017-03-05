bcc46f5027f7ff3cfa3e1391085c80781675f60a
Alexander Nikolov <al.a.nikolov@abv.bg>
2017-03-05 16:23:59 +0200

    Added 5 products via json files for each lower section for the first 2 main sections

--- Assets/JavaScript/mainJS.js
+++ Assets/JavaScript/mainJS.js
@@ -112,115 +112,115 @@ var sections = [
             new LowerSection('Мобилни телефони', 'phones'),
             new LowerSection('Таблети', 'tablets'),
             new LowerSection('Външни батерии', 'outer-batteries'),
-            new LowerSection('Аксесоари'),
-            new LowerSection('Смарт часовници'),
-            new LowerSection('Фитнес гривни'),
-            new LowerSection('Преносими тонколони'),
-            new LowerSection('Smart Home & VR очила')
+            new LowerSection('Аксесоари', 'accessories'),
+            new LowerSection('Смарт часовници', 'smartwatches'),
+            new LowerSection('Фитнес гривни', 'fitnessbrace'),
+            new LowerSection('Преносими тонколони', 'portable-speaker'),
+            new LowerSection('Smart Home & VR очила', 'smarthome-vr')
         ]
     },
     {
         section: new Section('Лаптопи, IT продукти & Офис'),
         lowerSections: [
-            new LowerSection('Лаптопи'),
-            new LowerSection('Чанти за лаптопи'),
-            new LowerSection('Хард дискове'),
-            new LowerSection('Охлоадителни Подложки'),
-            new LowerSection('Компютри'),
-            new LowerSection('Аксесоари за Компютри'),
-            new LowerSection('Офис столове'),
-            new LowerSection('Всичко за офиса')
+            new LowerSection('Лаптопи', 'laptops'),
+            new LowerSection('Чанти за лаптопи', 'lapbags'),
+            new LowerSection('Хард дискове', 'harddiscs'),
+            new LowerSection('Охлоадителни Подложки', 'cooling-pads'),
+            new LowerSection('Компютри', 'PCs'),
+            new LowerSection('Аксесоари за Компютри', 'acc-for-pc'),
+            new LowerSection('Офис столове', 'office-chairs'),
+            new LowerSection('Всичко за офиса', 'office-stuff')
         ]
     },
     {
         section: new Section('ТВ, Електроника & Фото'),
         lowerSections: [
-            new LowerSection('Телевизори'),
-            new LowerSection('Кабели и адаптери'),
-            new LowerSection('Система за домашно кино'),
-            new LowerSection('Усилватели'),
-            new LowerSection('Видео проектори'),
-            new LowerSection('MP3 и MP4 Плейъри'),
-            new LowerSection('Фотоапарати'),
-            new LowerSection('Видеокамери')
+            new LowerSection('Телевизори', 'TVs'),
+            new LowerSection('Кабели и адаптери', 'cables-adapters'),
+            new LowerSection('Система за домашно кино', 'home-cinema'),
+            new LowerSection('Усилватели', 'amplifiers'),
+            new LowerSection('Видео проектори', 'vid-proj'),
+            new LowerSection('MP3 и MP4 Плейъри', 'mp3-mp4'),
+            new LowerSection('Фотоапарати', 'cameras'),
+            new LowerSection('Видеокамери', 'video-cameras')
         ]
     },
     {
         section: new Section('Големи електроуреди'),
         lowerSections: [
-            new LowerSection('Хладилници'),
-            new LowerSection('Фризери'),
-            new LowerSection('Перални'),
-            new LowerSection('Сушилни'),
-            new LowerSection('Съдомиялни'),
-            new LowerSection('Печки'),
-            new LowerSection('Климатици'),
-            new LowerSection('Бойлери')
+            new LowerSection('Хладилници', 'refrigerators'),
+            new LowerSection('Фризери', 'Freezers'),
+            new LowerSection('Перални', 'washing-mach'),
+            new LowerSection('Сушилни', 'dryers'),
+            new LowerSection('Съдомиялни', 'dishwashers'),
+            new LowerSection('Печки', 'stoves'),
+            new LowerSection('Климатици', 'air-cond'),
+            new LowerSection('Бойлери', 'boilers')
         ]
     },
     {
         section: new Section('Малки електроуреди & Лична грижа'),
         lowerSections: [
-            new LowerSection('Прахосмукачки'),
-            new LowerSection('Кафе машини'),
-            new LowerSection('Месомелачки'),
-            new LowerSection('Хлебопекарни'),
-            new LowerSection('Сешоари'),
-            new LowerSection('Парфюми'),
-            new LowerSection('Епилатори'),
-            new LowerSection('Кремове')
+            new LowerSection('Прахосмукачки', 'vac-cleaners'),
+            new LowerSection('Кафе машини', 'coffee-mach'),
+            new LowerSection('Месомелачки', 'meat-grinders'),
+            new LowerSection('Хлебопекарни', 'breadmakers'),
+            new LowerSection('Сешоари', 'hairdryers'),
+            new LowerSection('Парфюми', 'perfumes'),
+            new LowerSection('Епилатори', 'epilators'),
+            new LowerSection('Кремове', 'skin-creams')
         ]
     },
     {
         section: new Section('Козметика, Мода & Спорт'),
         lowerSections: [
-            new LowerSection('Гримове'),
-            new LowerSection('Маникюр & педикюр'),
-            new LowerSection('Спортни обувки'),
-            new LowerSection('Спортно облекло'),
-            new LowerSection('Къмпинг'),
-            new LowerSection('Зимни спортове'),
-            new LowerSection('Бижута'),
-            new LowerSection('Часовници')
+            new LowerSection('Гримове', 'makeup'),
+            new LowerSection('Маникюр & педикюр', 'manicure-pedicure'),
+            new LowerSection('Спортни обувки', 'sports-shoes'),
+            new LowerSection('Спортно облекло', 'playwear'),
+            new LowerSection('Къмпинг', 'camping'),
+            new LowerSection('Зимни спортове', 'winter-sports'),
+            new LowerSection('Бижута', 'jewelry'),
+            new LowerSection('Часовници', 'watches')
         ]
     },
     {
         section: new Section('Книги, Детски артукули & Играчки'),
         lowerSections: [
-            new LowerSection('Художествена литература'),
-            new LowerSection('Световни романи'),
-            new LowerSection('Пелени'),
-            new LowerSection('Бебешки пюрета'),
-            new LowerSection('Залагалки'),
-            new LowerSection('Детски превозни средства'),
-            new LowerSection('Детски дрехи'),
-            new LowerSection('Играчки')
+            new LowerSection('Художествена литература', 'fiction-books'),
+            new LowerSection('Световни романи', 'best-novels'),
+            new LowerSection('Пелени', 'diapers'),
+            new LowerSection('Бебешки пюрета', 'purees'),
+            new LowerSection('Залъгалки', 'pacifiers'),
+            new LowerSection('Детски превозни средства', 'vehicles-for-children '),
+            new LowerSection('Детски дрехи', 'clothes-for-children'),
+            new LowerSection('Играчки', 'toys')
         ]
     },
     {
         section: new Section('Дом & Градина'),
         lowerSections: [
-            new LowerSection('Косачка'),
-            new LowerSection('Мебели'),
-            new LowerSection('Осветление'),
-            new LowerSection('Кухненски прибори'),
-            new LowerSection('Баня'),
-            new LowerSection('Кухния'),
-            new LowerSection('Спалния'),
-            new LowerSection('Хол')
+            new LowerSection('Косачка', 'mowers'),
+            new LowerSection('Мебели', 'furniture'),
+            new LowerSection('Осветление', 'lighting'),
+            new LowerSection('Кухненски прибори', 'tableware'),
+            new LowerSection('Баня', 'bathroom'),
+            new LowerSection('Кухния', 'kitchen'),
+            new LowerSection('Спалния', 'bedroom'),
+            new LowerSection('Хол', 'livingroom')
         ]
     },
     {
         section: new Section('Авто, Инструменти & Petshop'),
         lowerSections: [
-            new LowerSection('Инструменти'),
-            new LowerSection('Масло'),
-            new LowerSection('Застраховки'),
-            new LowerSection('Акумулатори'),
-            new LowerSection('Храна за любимци'),
-            new LowerSection('Играчки за любимци'),
-            new LowerSection('Къщички за любимци'),
-            new LowerSection('Каишки')
+            new LowerSection('Инструменти', 'tools'),
+            new LowerSection('Масло', 'oil'),
+            new LowerSection('Застраховки', 'insurance'),
+            new LowerSection('Акумулатори', 'accumulators'),
+            new LowerSection('Храна за любимци', 'pet-food'),
+            new LowerSection('Играчки за любимци', 'toys-for-pets'),
+            new LowerSection('Къщички за любимци', 'houses-for-pets'),
+            new LowerSection('Каишки', 'leashes')
         ]
     }
 ]
