5dd54a6cdac153bef8b58851853bbc42c4a1f185
Alexander Nikolov <al.a.nikolov@abv.bg>
2017-03-18 00:51:35 +0200

    Cart items can be displayed, added and removed now. Work in progress

--- Assets/JavaScript/cartpage.js
+++ Assets/JavaScript/cartpage.js
@@ -1,20 +1,27 @@
-function Cart() {
-    this.products = [];
+function Cart(products) {
+    this.products = products || [];
 }
 
 Cart.prototype.addProductToCart = function (product) {
-        this.products.push(product);
-},
+    this.products.push(product);
+}
 
-Cart.prototype.removeProductFromCart = function(index) {
+Cart.prototype.removeProductFromCart = function (index) {
     this.products.splice(index, 1);
 }
 
-Cart.prototype.getTotalPrice = function() {
+Cart.prototype.getTotalPrice = function () {
     var total = 0;
-    for (var index=0; index < this.products.length; index++) {
+    for (var index = 0; index < this.products.length; index++) {
         total += products[index].price;
     }
     return total;
 }
 
+
+
+function getUserSetCart() {
+    var obj = JSON.parse(sessionStorage.user);
+    obj.cart = new Cart(obj.cart.products);
+    return obj;
+}
--- Assets/JavaScript/productpage.js
+++ Assets/JavaScript/productpage.js
@@ -4,6 +4,8 @@ mainImage.src = sessionStorage.prodImage;
 var price = document.querySelector('#other>h2');
 price.textContent = sessionStorage.prodPrice;
 var supPrice = document.createElement('sup');
+supPrice.style.fontSize = "80%";
+supPrice.style.zIndex = "-1";
 supPrice.textContent = sessionStorage.prodSupPrice;
 price.appendChild(supPrice);
 var lv = document.createElement('span');
@@ -36,15 +38,22 @@ for (var index = 0; index < otherImages.length; index++) {
 
 var addToCart = document.querySelectorAll('#other>div');
 
-addToCart[1].addEventListener('click', function() {
-    user_id.cart.addProductToCart({
+addToCart[1].addEventListener('click', function () {
+    user.cart.addProductToCart({
         img: sessionStorage.prodImage,
         info: sessionStorage.prodInfo,
         price: sessionStorage.prodPrice,
         supPrice: sessionStorage.prodSupPrice
     })
-}, false)
+    //test user
+    sessionStorage.setItem('user', JSON.stringify(user));
+}, false);
 
-var user_id = {
-    cart: new Cart([])
+//test user
+var cart = new Cart([]);
+
+var user = {
+    cart: cart
 }
+sessionStorage.setItem('user', JSON.stringify(user));
+var user = getUserSetCart();
