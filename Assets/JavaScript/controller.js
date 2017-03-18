097313c426bc3d407a5bcf8cb4d17ddbc311d215
Mariana Dincheva <mpdincheva@gmail.com>
2017-03-18 08:50:21 +0200

    Profile pages finished

--- Assets/JavaScript/controller.js
+++ Assets/JavaScript/controller.js
@@ -28,10 +28,54 @@
             }, false);
 
         } else {
-            console.log("djoisn");
-            // register
+            console.log("msk");
+            document.getElementById("registrationForm").style.display = "inline-block";
+            document.getElementById("emailForm").style.display = "none";
+
+            document.getElementById('buttonSubmitRegistration').addEventListener('click', function(event) {
+                event.preventDefault();
+
+                var userpassword = document.getElementById("registerPassword");
+                var password = userpassword.value.trim();
+                var repeatedPassword = document.getElementById("repeatPassword");
+                var repeatPassword = repeatedPassword.value.trim();
+                var agree = document.getElementById('IAgree');
+                var agreeIsChecked = agree.checked;
+
+                if (password === repeatPassword && agreeIsChecked == true) {
+                    var user = new User(3, email, password);
+                    userManager.addUser(user);
+                    localStorage.setItem("user_id", user.id);
+                    window.location = "../HTML/index.html";
+                } else {
+                    if (document.getElementById('errorDifferentPasswords')) {
+                        document.getElementById('errorDifferentPasswords').parentElement.removeChild(document.getElementById('errorDifferentPasswords'));
+                        repeatedPassword.style.border = "1px solid grey";
+                        
+                    }
+
+                    if (document.getElementById('errorNotChecked')) {
+                        document.getElementById('errorNotChecked').parentElement.removeChild(document.getElementById('errorNotChecked'));
+                    }
+
+                    if (agreeIsChecked != true) {
+                        agree.style.border = "1px solid red";
+                        var errorIsChecked = document.createElement('div');
+                        errorIsChecked.innerHTML = "Не е отбелязано."
+                        errorIsChecked.id = "errorNotChecked";
+                        errorIsChecked.style.color = "red";
+                        agree.parentElement.appendChild(errorIsChecked);
+                    }
+                    if (password != repeatPassword){
+                        repeatedPassword.style.border = "1px solid red";
+                        var error = document.createElement('span');
+                        error.innerHTML = "Различни пароли"
+                        error.id = "errorDifferentPasswords";
+                        error.style.color = "red";
+                        repeatedPassword.parentElement.appendChild(error);
+                    }
+                }            
+            }, false);
         }
-
     });
-
 })();
